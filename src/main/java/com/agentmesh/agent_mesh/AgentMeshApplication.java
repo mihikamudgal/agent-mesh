8ca762ae90f1ae088f17b9909633a6af851511bd
mihikamudgal <mihika.mudgal05@gmail.com>
2026-08-14 23:05:36 +0530

    Initial project setup

---
+++ src/main/java/com/agentmesh/agent_mesh/AgentMeshApplication.java
@@ -0,0 +1,13 @@
+package com.agentmesh.agent_mesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentMeshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agentmesh/agent_mesh/AgentMeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentmesh.agent_mesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentMeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
