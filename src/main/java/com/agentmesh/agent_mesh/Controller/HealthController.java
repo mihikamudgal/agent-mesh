9f87d05e303b86d6e1ce286042eef29c7fef2bdc
mihikamudgal <mihika.mudgal05@gmail.com>
2026-08-25 23:25:15 +0530

    fix code

---
+++ src/main/java/com/agentmesh/agent_mesh/Controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.agentmesh.agent_mesh.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "Agent Mesh is healthy";
+    }
+}
